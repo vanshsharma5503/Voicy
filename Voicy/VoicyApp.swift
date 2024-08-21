586a1e13761fd17ed5e03ef33b8d3604c71b71b7
Vansh sharma <vanshsharma5503@gmail.com>
2024-08-21 19:13:17 +0530

    Initial Commit

---
+++ Voicy/VoicyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoicyApp.swift
+//  Voicy
+//
+//  Created by vansh sharma on 21/08/24.
+//
+
+import SwiftUI
+
+@main
+struct VoicyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
